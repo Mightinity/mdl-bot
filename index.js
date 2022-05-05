de08544cfa1e48f49d35757f94c6ecb4a0f88707
Mightinity <64143394+Mightinity@users.noreply.github.com>
2022-05-06 04:09:19 +0700

    initialize to github repository

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js")
+require("dotenv").config()
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "maya")
+        message.reply("Maya Debora Limbong")
+})
+
+client.login(process.env.TOKEN)
